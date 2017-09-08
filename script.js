18897ab8148ba21f64d8be9c2e07ff19b1529994
Djonikeee <djoni139@gmail.com>
2017-09-08 09:37:22 +0300

    Add files via upload
    added project files

---
+++ script.js
@@ -0,0 +1,21 @@
+var Vref = 1.25;
+var Iadj = 0.00005;
+
+
+function VCalculate()
+{
+    if (parseInt(document.Vout.Res2.value) / parseInt(document.Vout.Res1.value) > 10.2)
+        {
+            alert("Vout > 14 Вольт! Примените резистор R2 меньшего номинала.");
+            document.Vout.VoutResult.value='';
+            return false;
+        }
+    if (parseInt(document.Vout.Res1.value) < 75 && parseInt(document.Vout.Res1.value) > 270)
+        {
+            alert("Резистор R1 должен иметь сопротивление от 75 до 270 Ом");
+            document.Vout.VoutResult.value='';
+            return false;
+        }    
+    document.Vout.VoutResult.value = Vref*(1+(parseInt(document.Vout.Res2.value)/parseInt(document.Vout.Res1.value)))+(Iadj*parseInt(document.Vout.Res2.value));
+    return true;
+}
