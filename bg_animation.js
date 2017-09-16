48542470813563f740df908bd196ecd65dc5a83e
Djonikeee <djoni139@gmail.com>
2017-09-16 23:12:07 +0300

    ---

---
+++ bg_animation.js
@@ -0,0 +1,31 @@
+var canvasBody = document.getElementById("canvas"); 
+var canvas = canvasBody.getContext("2d");
+var w = canvasBody.width = window.innerWidth; //задается ширина на весь экран
+var h = canvasBody.height = window.innerHeight; //задается высота на весь экран
+
+var opts = {
+    count: 0.5,
+    size: 30, //размер объектов в пикселах
+    sizeRandom: 10, //рандомное значение к размеру объектов (от 0 до 10)
+    spawnOpacity: 1, //прозрачность объектов 
+    sparkLife: 0.1, //время жизни объекта
+    color: "rgba(39, 173, 96, alpha)" //цвет объектов
+};
+
+function anim(){
+    window.requestAnimationFrame(anim);
+    step();
+}
+
+anim();
+
+function step(){
+    var fillColor = opts.color.replace("alpha", opts.spawnOpacity);
+    canvas.fillStyle = fillColor;
+    for(var i = 0; i < Math.round(opts.count); i++){
+        var random = Math.random() * opts.sizeRandom;
+        canvas.fillRect(- (opts.size / 2) + Math.random() * w, - (opts.size / 2) + Math.random() * h, opts.size + random, opts.size + random);
+    }
+    canvas.fillStyle = "rgba(255,255,255,"+ opts.sparkLife +")";
+    canvas.fillRect(0, 0, w, h);
+}
